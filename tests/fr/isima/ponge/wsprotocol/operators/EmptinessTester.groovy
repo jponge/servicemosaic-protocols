4b96fc48b97af8b57dd9e67a529610b383528c75
Julien Ponge <ponge@isima.fr>
2008-07-18 18:46:00 +0000

    Swapped the header and the imports.

--- tests/fr/isima/ponge/wsprotocol/operators/EmptinessTester.groovy
+++ tests/fr/isima/ponge/wsprotocol/operators/EmptinessTester.groovy
@@ -1,30 +1,30 @@
+/*
+ * Copyright 2005-2008 Julien Ponge <http://julien.ponge.info/>.
+ * Copyright 2005-2008 Universite Blaise Pascal, LIMOS, Clermont-Ferrand, France.
+ * Copyright 2005-2008 The University of New South Wales, Sydney, Australia.
+ *
+ * This file is part of ServiceMosaic Protocols <http://servicemosaic.isima.fr/>.
+ *
+ * ServiceMosaic Protocols is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ServiceMosaic Protocols is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ServiceMosaic Protocols.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 import fr.isima.ponge.wsprotocol.BusinessProtocol
 import fr.isima.ponge.wsprotocol.BusinessProtocolFactory
 import fr.isima.ponge.wsprotocol.Polarity
 import fr.isima.ponge.wsprotocol.StandardExtraProperties
 import fr.isima.ponge.wsprotocol.impl.BusinessProtocolFactoryImpl
 import fr.isima.ponge.wsprotocol.operators.EmptinessOperator
-
-/*
-* Copyright 2005-2008 Julien Ponge <http://julien.ponge.info/>.
-* Copyright 2005-2008 Universite Blaise Pascal, LIMOS, Clermont-Ferrand, France.
-* Copyright 2005-2008 The University of New South Wales, Sydney, Australia.
-*
-* This file is part of ServiceMosaic Protocols <http://servicemosaic.isima.fr/>.
-*
-* ServiceMosaic Protocols is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ServiceMosaic Protocols is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with ServiceMosaic Protocols.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 /*
  * This is not implemented as a JUnit test case as there is a dependency
